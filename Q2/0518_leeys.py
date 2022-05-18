71031b8fcbcf781ef01c305f69256313613a44fa
yeonseok <leeys4140@naver.com>
2022-05-19 01:29:19 +0900

    Deciding 'Prime Number'

--- Q2/0518_leeys.py
+++ Q2/0518_leeys.py
@@ -1,5 +1,13 @@
-function_list = list(map(int, input().split()))
-speeds = list(map(int, input().split()))
+num = int(input("정수 입력 : "))
+state = 1
 
-c = [function_list[i] + speeds[i] for i in range(len(int(function_list)))]
-print(c)
+for i in range(2, num):
+    if num % i == 0:
+        break
+    else:
+        state = 0
+
+if state == 1:
+    print("%d는 소수가 아닙니다." % num)
+else:
+    print("%d는 소수입니다." % num)
