939afd144e58699bf8b4db1bb5efb6f190cceb46
Tommy <jtaofi0129@naver.com>
2022-05-25 01:24:57 +0900

    Dictionary 함수 구문 숙지하면 곧 풀 것 같다.

--- Q4/0524_kdi.py
+++ Q4/0524_kdi.py
@@ -1,25 +1,25 @@
-
-
+minute = 0
 
 No_Jommel = {1:0, 2:0, 3:0, 4:0, 5:0}
 
-number = int(input('점멸 빠진 사람의 번호를 입력하시오 : '))
-
-if number == 0:
-elif number == 6:
-    break
-else:
-    No_Jommel.get(number) = 5
-
-
-
-
-
-
-
-
-
-
-
-
 while True:
+    minute += 1
+    number = int(input('점멸 빠진 사람의 번호를 입력하시오 : '))
+
+    if number == 0:
+        continue
+    elif number == 6:
+        break
+    else:
+        No_Jommel.get[number] = 5
+
+    for key in No_Jommel:
+        if No_Jommel[number] != 0:
+            No_Jommel.get[number] -= 1
+
+    if No_Jommel[number] = 0:
+        state = "on"
+    else
+        state = "off"
+
+    print("%d분 : 1:%s, 2:%s, 3:%s, 4:%s, 5:%s" %(minute)
