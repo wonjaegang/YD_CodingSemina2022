d3df0d991b98c46bed20537b3e74174fc1fcb2b6
Tommy <jtaofi0129@naver.com>
2022-05-25 00:57:51 +0900

    Dictionary를 이용해 풀 수 있을 것 같아 시도 중이다.

---
+++ Q4/0524_kdi.py
@@ -0,0 +1,25 @@
+
+
+
+No_Jommel = {1:0, 2:0, 3:0, 4:0, 5:0}
+
+number = int(input('점멸 빠진 사람의 번호를 입력하시오 : '))
+
+if number == 0:
+elif number == 6:
+    break
+else:
+    No_Jommel.get(number) = 5
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
