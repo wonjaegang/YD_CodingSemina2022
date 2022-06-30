696ed3b0b7d4a9bb1787359f788ec07aea224b95
Tommy <jtaofi0129@naver.com>
2022-06-30 13:09:22 +0900

    a+b / a+c 문제 해결

--- Q6/0625_kdi(a+b).py
+++ Q6/0625_kdi(a+b).py
@@ -1,8 +1,10 @@
 H, W = map(int, input('원하는 H와 W의 크기를 각각 입력하시오').split(','))
 
-# 원하는 열(H), 행(W)만큼 크기의 grid라는 H개의 리스트를 포함하는 다차원 배열 생성.
-
+# 원하는 열(H), 행(W)만큼 크기의 grid라는 H개의 리스트를 포함하는 'O'를 원소로 하는 다차원 배열 생성.
 grid = [['O' for _ in range(W)] for _ in range(H)]
+
+# grid와 같은 크기의 'X'를 원소로 하는 다차원 배열 생성
+compare_grid = [['X' for col in range(W)] for row in range(H)]
 
 rows = len(grid)
 cols = len(grid[0])
@@ -14,7 +16,7 @@ for r in range(rows):
         print(grid[r][c], end=" ")
     print()
 
-# 원하는 좌표칸 뒤집기
+# 원하는 좌표 칸 뒤집기
 
 while(1):
     h, w = map(int, input('뒤집을 격자판의 좌표(h, w)를 입력하시오').split(','))
@@ -31,10 +33,6 @@ while(1):
 
 # 다 뒤집히면 게임 끝.
 
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 'X':
-                pass
-            pass
+    if grid == compare_grid:
         break
 
--- Q6/0625_kdi(a+c).py
+++ Q6/0625_kdi(a+c).py
@@ -1,8 +1,10 @@
 H, W = map(int, input('원하는 H와 W의 크기를 각각 입력하시오').split(','))
 
 # 원하는 열(H), 행(W)만큼 크기의 grid라는 H개의 리스트를 포함하는 다차원 배열 생성.
-
 grid = [['O' for _ in range(W)] for _ in range(H)]
+
+# grid와 같은 크기의 'X'를 원소로 하는 다차원 배열 생성
+compare_grid = [['X' for col in range(W)] for row in range(H)]
 
 rows = len(grid)
 cols = len(grid[0])
@@ -14,23 +16,44 @@ for r in range(rows):
         print(grid[r][c], end=" ")
     print()
 
-# 원하는 좌표 기준포함 상하좌우 칸 뒤집기
+# 원하는 좌표 포함 상하좌우 칸 뒤집기
 
 while(1):
     h, w = map(int, input('뒤집을 격자판의 좌표(h, w)를 입력하시오').split(','))
 
+    # 원하는 좌표 칸
     if grid[h - 1][w - 1] == 'O':
-        grid[h - 1][w - 1] = 'X'            # 원하는 좌표 칸
-        if h - 2 >= 0:
-            if grid[h - 2][w - 1] == 'O':
-             grid[h - 2][w - 1] = 'X'  # 윗칸
-            else:
-                grid[h - 2][w - 1] = 'O'
-        if h + 2 <= H:
-
-
+        grid[h - 1][w - 1] = 'X'
     else:
-        grid[h-1][w-1] = 'O'
+        grid[h - 1][w - 1] = 'O'
+
+    # 윗칸
+    if h - 2 >= 0:
+        if grid[h - 2][w - 1] == 'O':
+         grid[h - 2][w - 1] = 'X'
+        else:
+         grid[h - 2][w - 1] = 'O'
+
+    # 아래칸
+    if h <= H:
+        if grid[h][w - 1] == 'O':
+         grid[h][w - 1] = 'X'
+        else:
+         grid[h][w - 1] = 'O'
+
+    # 좌측칸
+    if w - 2 >= 0:
+        if grid[h - 1][w - 2] == 'O':
+         grid[h - 1][w - 2] = 'X'
+        else:
+         grid[h - 1][w - 2] = 'O'
+
+    # 우측칸
+    if w <= W:
+        if grid[h - 1][w] == 'O':
+         grid[h - 1][w] = 'X'
+        else:
+         grid[h - 1][w] = 'O'
 
     for r in range(rows):
         for c in range(cols):
@@ -38,11 +61,5 @@ while(1):
         print()
 
 # 다 뒤집히면 게임 끝.
-
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 'X':
-                pass
-            pass
+    if grid == compare_grid:
         break
-
