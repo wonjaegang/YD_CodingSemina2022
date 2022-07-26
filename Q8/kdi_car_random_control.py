82e8d35102d8fdae88b1eedb07f10d92cf1c478d
dooin <jtaofi0129@naver.com>
2022-07-26 23:24:56 +0900

    car_control 정사각형 문제 해결 / random 풀이 중

---
+++ Q8/kdi_car_random_control.py
@@ -0,0 +1,92 @@
+import math
+from math import *
+
+import pygame
+from pygame.locals import *
+
+import random
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+pygame.init()
+WIDTH = 500
+HEIGHT = 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+rate = 50
+
+
+class Obstacle:
+    def __init__(self, initial_location, size):
+        self.rect = pygame.rect.Rect(0, 0, *size)
+        self.x, self.y = initial_location
+
+    def GUI_display(self):
+        self.rect.centerx = self.x
+        self.rect.centery = 500 - self.y
+        pygame.draw.rect(screen, BLACK, self.rect)
+
+
+# 주어진 속력과 방향에 따라 등속운동하는 자동차
+# 좌표계의 좌측 하단이 0, 0 이다. 우측 방향이 x+, 위쪽 방향이 y+.
+class Car:
+    def __init__(self, initial_location):
+        self.x, self.y = initial_location
+        self.direction = 0
+        self.velocity = 1
+
+        self.size = 10
+        self.rect = pygame.rect.Rect(0, 0, self.size, self.size)
+
+    def move(self):
+        self.x += self.velocity * cos(self.direction)
+        self.y += self.velocity * sin(self.direction)
+
+    def GUI_display(self):
+        self.rect.centerx = self.x
+        self.rect.centery = 500 - self.y
+        pygame.draw.rect(screen, BLACK, self.rect)
+
+    def set_velocity(self, count):
+        self.velocity = 1
+
+    def set_steer(self, count):
+        print(count)
+        if 0 < self.x < 500 and 0 < self.y < 500:
+            if count // 150 == 0:
+              self.direction = random.randrange(0, 7)
+            else:
+                self.direction = self.direction
+        else:
+            self.direction = abs(math.pi - self.direction)
+
+    def Lidar(self):
+        pass
+
+
+def main():
+    car = Car([250, 125])  # x, y
+    obstacle = Obstacle([250, 250], [50, 10])  # (x, y), (width, height)
+    count = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return 0
+
+        car.set_velocity(count)
+        car.set_steer(count)
+        car.move()
+
+        screen.fill(WHITE)
+        car.GUI_display()
+        obstacle.GUI_display()
+
+        pygame.display.flip()
+        clock.tick(rate)
+        count += 1
+
+
+if __name__ == '__main__':
+    main()
