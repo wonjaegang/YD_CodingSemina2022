3a30738f7f69d76d865465251d6678e31de81c03
Wonjaegang <robinsam@naver.com>
2022-07-30 19:21:51 +0900

    디렉토리명 변경

--- Q9/Car_Control.py
+++ Q9/Car_Control.py
@@ -1,13 +1,5 @@
 """
-주어진 함수 print_grid_map() 을 완성해보자.
-
-print_grid_map()은 가상의 라이다 virtual_lidar() 로부터 1도의 간격으로 거리값이 담긴 길이 360개의 거리값을 배열로 받는다.
-그 후, 10X10 그리드맵을 출력한다. 장애물(마우스)가 존재하는 좌표는 X로, 아닌 좌표는 O로 나타내어보자.
-
-그리드맵의 실제크기는 500X500이며, 그리드맵의 중심에 라이다가 존재한다.
-중심기준 오른쪽 수평직선에서 0도로 시작하고, 반시계 방향으로 359까지 증가한다.
-
-단, print_grid_map() 에 인자로 주어진 sensor_data 의 값만 사용하자. 가공할 수 있는 값들은 sensor_data 의 360개의 값이 전부다.
+자동차를 1) 정사각형 경로로 2) 랜덤 경로로 움직여보자.
 
 """
 
@@ -39,6 +31,7 @@ class Obstacle:
 
 # 주어진 속력과 방향에 따라 등속운동하는 자동차
 # 좌표계의 좌측 하단이 0, 0 이다. 우측 방향이 x+, 위쪽 방향이 y+.
+# direction 은 절대좌표계를 따른다.
 class Car:
     def __init__(self, initial_location):
         self.x, self.y = initial_location
@@ -58,7 +51,7 @@ class Car:
         pygame.draw.rect(screen, BLACK, self.rect)
 
     def set_velocity(self, count):
-        self.velocity = 10
+        self.velocity = 1
 
     def set_steer(self, count):
         self.direction = count / 100
