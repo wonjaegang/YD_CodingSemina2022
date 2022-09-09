7fc42b650d2d2ceddbe68e0231d3dece7a9c1082
Tommy <jtaofi0129@naver.com>
2022-09-10 00:09:08 +0900

    dwa 진행중 . 예상되는 속도 값들 카피코딩 시도.

--- Q13/kdi_evolved_dwa.py
+++ Q13/kdi_evolved_dwa.py
@@ -160,40 +160,21 @@ class Car:
         return obstacles
 
     def DWA(self):
-        w1 = 1
-        w2 = 1
-        w3 = 0
-        search_frame = 10
-        obstacle_force = 100
-        obstacles = self.obstacles()
+        # 미래 속도 값 정하기
+        DWA_size = 5
+        DWA_step = 2 * self.max_a / (DWA_size - 1)
+        DWA_right_v = [self.right_wheel - self.max_a + DWA_step * i for i in range(DWA_size) if
+                       (self.right_wheel - self.max_a + DWA_step * i) <= self.max_v]
+        DWA_left_v = [self.left_wheel - self.max_a + DWA_step * i for i in range(DWA_size) if
+                      (self.left_wheel - self.max_a + DWA_step * i) <= self.max_v]
+        # 미래 속도 값 정하기 끝
 
-        def cost_function(pos):
-            cost1 = sqrt((pos[0] - self.goal_x) ** 2 + (pos[1] - self.goal_y) ** 2)
-            obs_d = min([sqrt((pos[0] - obstacle[0]) ** 2 + (pos[1] - obstacle[1]) ** 2) for obstacle in obstacles])
-            cost2 = obstacle_force - obs_d if obs_d < obstacle_force else 0
-            return cost1 * w1 + cost2 * w2
+        weight1 = 1
+        weight2 = 1
 
-        DWA_search_size = 5
-        DWA_step = 2 * self.max_a / (DWA_search_size - 1)
-        DWA_right = [self.right_wheel - self.max_a + DWA_step * i for i in range(DWA_search_size) if
-                     abs(self.right_wheel - self.max_a + DWA_step * i) <= self.max_v]
-        DWA_left = [self.left_wheel - self.max_a + DWA_step * i for i in range(DWA_search_size) if
-                    abs(self.left_wheel - self.max_a + DWA_step * i) <= self.max_v]
+        # 미래의 위치 값을 받아야함 만들어진 속도들을 바탕으로 각각을 get.velocity 함수에 넣어 파악할 수 있다. -> x, y 좌표와 헤딩을,,
 
-        best_cost = float('inf')
-        best_vel = [self.right_wheel, self.left_wheel]
-        for right_wheel in DWA_right:
-            for left_wheel in DWA_left:
-                pos_after = [self.x, self.y, self.heading]
-                for _ in range(search_frame):
-                    vel = self.get_velocity(pos_after[-1], right_wheel, left_wheel)
-                    pos_after = [pos + vel for pos, vel in zip(pos_after, vel)]
-                cost = cost_function(pos_after[:2])
-                if cost < best_cost:
-                    best_cost = cost
-                    best_vel = [right_wheel, left_wheel]
-                pygame.draw.circle(screen, BLUE, [round(pos_after[0]), round(Y_MAX - pos_after[1])], 3)
-        return best_vel
+
 
 
 def main():
